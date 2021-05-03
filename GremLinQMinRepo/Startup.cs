b0744ab8d884d1eed79b53cc6e34129fa149294f
Dmo16 <45639728+Dmo16@users.noreply.github.com>
2021-05-03 13:10:06 -0400

    Update Startup.cs

--- GremLinQMinRepo/Startup.cs
+++ GremLinQMinRepo/Startup.cs
@@ -18,10 +18,9 @@ namespace GremLinQMinRepo
                         .ConfigureProperties(model =>
                             model.ConfigureElement<Vertex>(config => config.IgnoreOnUpdate(x => x.group ))))
                     .UseCosmosDb(builder => builder
-                        .At(new Uri("wss://public-tests.gremlin.cosmos.azure.com:443/"), "graphdb",
-                            "gremlinq-tests")
+                        .At(YOUR_CONNECTION_HERE)
                         .AuthenticateBy(
-                            "I13DBY35jCFksC4Knv316J4q6UkvdyVtZ9BMpkq1QRbEWnfq8pmpo9Z1S84AsLemQbX4ghqnRWa3ryS1uLlr9w=="))));
+                            YOUR_KEY_HERE))));
         }
     }
 }
